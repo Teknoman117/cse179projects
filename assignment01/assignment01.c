6cdf5473b79a4072ef2dd0ae46b8e90c4bae89ff
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2015-03-02 08:06:14 -0800

    Finalized assignment 1

--- assignment01/assignment01.c
+++ assignment01/assignment01.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <time.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 #include <mpi.h>
 
 #define MASTER_PROCESS     0
-#define TAG_STATUS_MESSAGE 0z
+#define TAG_STATUS_MESSAGE 0
 #define CELL_STATUS_DEAD   0
 #define CELL_STATUS_ALIVE  1
 
@@ -90,11 +91,14 @@ int main(int argc, char *argv[])
     char            *initialGrid    = NULL;
     char            *currentGrid    = NULL;
     char             localCellValue = 0;
+    struct timeval   start;
+    struct timeval   end;
 
     // Constuct initial grid in master process
     if(processId == MASTER_PROCESS)
     {
         // Allocate the storage grids.  initial is for the random starting state, current is for debug and final grid display
+        gettimeofday(&start, NULL);
         initialGrid = (char *) calloc (gridWidth * gridHeight, sizeof(char));
         currentGrid = (char *) calloc (gridWidth * gridHeight, sizeof(char));
         for(int i = 0; i < gridWidth * gridHeight; i++)
@@ -108,43 +112,60 @@ int main(int argc, char *argv[])
     MPI_Scatter((void *) initialGrid, 1, MPI_CHAR, (void *) &localCellValue, 1, MPI_CHAR, MASTER_PROCESS, MPI_COMM_WORLD);
 
     // Compute a list of neighbors of this cell (their process ids)
-    int  neighbors[8]     = {-1, -1, -1, -1, -1, -1, -1, -1};
-    char neighborState[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    int  neighborCount    = 0;
-    for(int i = -1; i <= 1; i++)
+    int  neighborDirections[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int  neighbors[4]             = {-1, -1, -1, -1};
+
+    // Figure out if these neighbors exist
+    for(int i = 0; i < 4; i++)
     {
-        for(int j = -1; j <= 1; j++)
+        // If this neighbor is located within the grid
+        struct GridCell neighbor =
         {
-            // If this neighbor is located within the grid
-            struct GridCell neighbor =
-            {
-                .x = localCell.x + j,
-                .y = localCell.y + i
-            };
-            if(neighbor.x >= 0 && neighbor.x < gridWidth &&
-               neighbor.y >= 0 && neighbor.y < gridHeight &&
-               !(i == 0 && j == 0))
-            {
-                neighbors[neighborCount++] = ConvertToProcessId(neighbor, gridWidth, gridHeight);
-            }
+            .x = localCell.x + neighborDirections[i][0],
+            .y = localCell.y + neighborDirections[i][1]
+        };
+        if(neighbor.x >= 0 && neighbor.x < gridWidth &&
+           neighbor.y >= 0 && neighbor.y < gridHeight)
+        {
+            neighbors[i] = ConvertToProcessId(neighbor, gridWidth, gridHeight);
         }
     }
 
     // Perform the main iterations of the game of life simulation
+    MPI_Request request;
     for(int iteration = 0; iteration < iterationCount; iteration++)
     {
-        // Send our state to all the surrounding neighbors
-        MPI_Request request;
-        for(int i = 0; i < neighborCount; i++)
-            MPI_Isend((void *) &localCellValue, 1, MPI_CHAR, neighbors[i], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, &request);
+        // 0 = left, 1 = right, 2,3,4 = up{left,center,right}, 5,6,7 = down{left,center,right}
+        char neighborState[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-        // Receive all surrounding cell values
+        // Send our state to the neighbors left and right of us
+        if(neighbors[0] >= 0)
+            MPI_Isend((void *) &localCellValue, 1, MPI_CHAR, neighbors[0], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, &request);
+        if(neighbors[1] >= 0)
+            MPI_Isend((void *) &localCellValue, 1, MPI_CHAR, neighbors[1], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, &request);
+        if(neighbors[0] >= 0)
+            MPI_Recv((void *) &neighborState[0], 1, MPI_CHAR, neighbors[0], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if(neighbors[1] >= 0)
+            MPI_Recv((void *) &neighborState[1], 1, MPI_CHAR, neighbors[1], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Send the combined states above and below
+        char intermediate[3] = {neighborState[0], localCellValue, neighborState[1]};
+        //char intermediate[3] = {0, localCellValue, 0};  // play conway
+
+        // Send our state to the neighbors left and right of us
+        if(neighbors[2] >= 0)
+            MPI_Isend((void *) &intermediate[0], 3, MPI_CHAR, neighbors[2], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, &request);
+        if(neighbors[3] >= 0)
+            MPI_Isend((void *) &intermediate[0], 3, MPI_CHAR, neighbors[3], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, &request);
+        if(neighbors[2] >= 0)
+            MPI_Recv((void *) &neighborState[2], 3, MPI_CHAR, neighbors[2], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if(neighbors[3] >= 0)
+            MPI_Recv((void *) &neighborState[5], 3, MPI_CHAR, neighbors[3], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Count the number of alive cells
         int liveNeighborCount = 0;
-        for(int i = 0; i < neighborCount; i++)
-        {
-            MPI_Recv((void *) &neighborState[i], 1, MPI_CHAR, neighbors[i], TAG_STATUS_MESSAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        for(int i = 0; i < 8; i++)
             liveNeighborCount = (neighborState[i] == CELL_STATUS_ALIVE) ? liveNeighborCount + 1 : liveNeighborCount;
-        }
 
         // Game of life logic
         if(localCellValue == CELL_STATUS_ALIVE)
@@ -181,10 +202,14 @@ int main(int argc, char *argv[])
     MPI_Gather((void *) &localCellValue, 1, MPI_CHAR, (void *) currentGrid, 1, MPI_CHAR, MASTER_PROCESS, MPI_COMM_WORLD);
     if(processId == MASTER_PROCESS)
     {
+        gettimeofday(&end, NULL);
         PrintGrid(currentGrid, gridWidth, gridHeight, "Final");
         free(initialGrid);
         free(currentGrid);
+
+        printf("Execution Took: %f seconds\n", (float)((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)) / 1000000.0f);
     }
 
     MPI_Finalize();
+    return 0;
 }
